0ebbf0c4dc7af7ef4adb803f867f74fc00a0ba09
sergeisaz <almagroup2019@gmail.com>
2021-10-09 19:34:02 +0300

    add cors

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: "pinfognjoqksct",
-  host: "ec2-52-7-228-45.compute-1.amazonaws.com",
-  database: "d6vqdto88ecej",
-  password: "236ac34fce57dc0a4eb114d42ca0bdef5d88e7a5e81369ed110edcc9c50d0f01",
-  port: 5432,
-});
+require("dotenv").config();
+
+const devConfig = {
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+};
+
+const proConfig = {
+  connectionString: process.env.PG_DATABASE_URL,
+};
+
+const pool = new Pool({ proConfig });
 
 pool.connect();
 
--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const userRouter = require("./routes/user.routes");
 const postRouter = require("./routes/post.routes");
 const PORT = process.env.PORT || 8080;
-
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", postRouter);
 app.use("/api", userRouter);
