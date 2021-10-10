6e46caacf7020df8a664a50097d4ca5ce36fc65b
sergeisaz <almagroup2019@gmail.com>
2021-10-10 14:38:31 +0300

    setting bd

--- db.js
+++ db.js
@@ -9,11 +9,19 @@ const devConfig = {
   port: process.env.PG_PORT,
 };
 
+// const developConf = {
+//   user: "postgres",
+//   host: "localhost",
+//   database: "postgres",
+//   password: "root",
+//   port: 5432,
+// };
+
 const proConfig = {
   connectionString: `postgres://pinfognjoqksct:236ac34fce57dc0a4eb114d42ca0bdef5d88e7a5e81369ed110edcc9c50d0f01@ec2-52-7-228-45.compute-1.amazonaws.com:5432/d6vqdto88ecej`,
 };
 
-const pool = new Pool({ proConfig });
+const pool = new Pool(proConfig);
 
 pool.connect();
 
