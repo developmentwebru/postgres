bd38f099482c5a6daff12cb11aac5bff3a420eae
sergeisaz <almagroup2019@gmail.com>
2021-10-10 14:50:46 +0300

    test

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,16 +2,24 @@ const db = require("../db");
 
 class UserController {
   async createUser(req, res) {
-    const { name, surname } = req.body;
-    const newPerson = await db.query(
-      `INSERT INTO person (name, surname) values ($1, $2) RETURNING *`,
-      [name, surname]
-    );
-    res.json(newPerson.rows[0]);
+    try {
+      const { name, surname } = req.body;
+      const newPerson = await db.query(
+        `INSERT INTO person (name, surname) values ($1, $2) RETURNING *`,
+        [name, surname]
+      );
+      res.json(newPerson.rows[0]);
+    } catch (error) {
+      console.log(error);
+    }
   }
   async getUsers(req, res) {
-    const users = await db.query(`SELECT * FROM person`);
-    res.json(users.rows);
+    try {
+      const users = await db.query(`SELECT * FROM person`);
+      res.json(users.rows);
+    } catch (error) {
+      console.log(error);
+    }
   }
   async getOneUser(req, res) {
     const id = req.params.id;
