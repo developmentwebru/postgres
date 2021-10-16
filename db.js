0b3b51d5fec66843ea96672dc439af4ba55dce55
sergeisaz <almagroup2019@gmail.com>
2021-10-16 16:24:16 +0300

    test5

--- db.js
+++ db.js
@@ -17,12 +17,21 @@ const devConfig = {
 //   port: 5432,
 // };
 
-const proConfig = {
-  connectionString: DATABASE_URL,
+// const proConfig = {
+//   connectionString: DATABASE_URL,
+//   ssl: { rejectUnauthorized: false },
+// };
+
+const stringproCongig = {
   ssl: { rejectUnauthorized: false },
+  user: "pinfognjoqksct",
+  host: "ec2-52-7-228-45.compute-1.amazonaws.com",
+  database: "d6vqdto88ecej",
+  password: "236ac34fce57dc0a4eb114d42ca0bdef5d88e7a5e81369ed110edcc9c50d0f01",
+  port: 5432,
 };
 
-const pool = new Pool(proConfig);
+const pool = new Pool(stringproCongig);
 
 pool.connect();
 
